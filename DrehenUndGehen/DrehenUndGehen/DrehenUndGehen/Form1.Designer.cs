242d1b5fd5c9f0998e75801b6609948f77a501f0
Syrinata <david-strunk@online.de>
2015-05-31 12:09:39 +0200

    Markierung der Verschiebbaren Reihen und Spalten
    Die Reihen die verschoben werden können werden jetzt bei jeder
    Map/Mappoint-Größe markiert.
    Die Picturebox erlaubt jetzt Drag&Drop.
    Vereinzelt wurden ein Paar Übergabeparameter geändert wenn dies nötig
    war.

---
+++ DrehenUndGehen/DrehenUndGehen/DrehenUndGehen/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DrehenUndGehen
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1379, 904);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+
+
+    }
+}
+
---
+++ DrehenUndGehen/DrehenUndGehen/DrehenUndGehen/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DrehenUndGehen
+{
+    public partial class Form1 : Form
+    {
+        
+        Graphics g; 
+        Map first;
+        Renderer rend;
+        private bool _moving;
+        private Point _startLocation;
+
+        public Form1()
+        {
+            InitializeComponent();
+            first = new Map(9);
+            first.fillMap(75);           
+            WindowState = FormWindowState.Maximized;
+            g = this.CreateGraphics();
+            rend = new Renderer(first, g,pictureBox1);
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            rend.drawMap(75,this);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            
+          
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            first.exchangeCard.switchPosition();
+            pictureBox1.BackgroundImage = first.exchangeCard.looks;
+            
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _moving = true;
+            _startLocation = e.Location;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _moving = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_moving)
+            {
+                pictureBox1.Left += e.Location.X - _startLocation.X;
+                pictureBox1.Top += e.Location.Y - _startLocation.Y;
+            }
+        }
+        
+      
+
+  
+
+    }
+
+}
