74dda44246553d1f1a8277dffd3ab77bf5e9c35d
emdown <romanbauer3688@yahoo.de>
2015-06-22 22:40:22 +0200

    coint idee

--- DrehenUndGehen/WhoBegins.Designer.cs
+++ DrehenUndGehen/WhoBegins.Designer.cs
@@ -28,7 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnGameStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnGameStart
@@ -41,15 +45,30 @@
             this.btnGameStart.UseVisualStyleBackColor = true;
             this.btnGameStart.Click += new System.EventHandler(this.btnGameStart_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(92, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(338, 342);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
             // WhoBegins
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 562);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnGameStart);
             this.Name = "WhoBegins";
             this.Text = "WhoBegins";
             this.Load += new System.EventHandler(this.WhoBegins_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,5 +76,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnGameStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- DrehenUndGehen/WhoBegins.cs
+++ DrehenUndGehen/WhoBegins.cs
@@ -11,9 +11,26 @@ namespace DrehenUndGehen
 {
     public partial class WhoBegins : Form
     {
+        int i;
+        Bitmap[] coin;
         public WhoBegins()
         {
             InitializeComponent();
+            coin = new Bitmap[12];
+            coin[0] = new Bitmap(@"coin\IMG00000.bmp");
+            coin[1] = new Bitmap(@"coin\IMG00001.bmp");
+            coin[2] = new Bitmap(@"coin\IMG00002.bmp");
+            coin[3] = new Bitmap(@"coin\IMG00003.bmp");
+            coin[4] = new Bitmap(@"coin\IMG00004.bmp");
+            coin[5] = new Bitmap(@"coin\IMG00005.bmp");
+            coin[6] = new Bitmap(@"coin\IMG00006.bmp");
+            coin[7] = new Bitmap(@"coin\IMG00007.bmp");
+            coin[8] = new Bitmap(@"coin\IMG00008.bmp");
+            coin[9] = new Bitmap(@"coin\IMG00009.bmp");
+            coin[10] = new Bitmap(@"coin\IMG00010.bmp");
+            coin[11] = new Bitmap(@"coin\IMG00011.bmp");
+            i = 0;
+
         }
 
         private void WhoBegins_Load(object sender, EventArgs e)
@@ -29,14 +46,28 @@ namespace DrehenUndGehen
 
         private void btnGameStart_Click(object sender, EventArgs e)
         {
-            this.Visible = false;
-            Countdown count = new Countdown();
-            count.ShowDialog();
+            timer1.Enabled = true;
+        }
 
-            GameLoading game = new GameLoading();
-            game.Show();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i++;
+            if (i == 12)
+                i = 0;
+            timer1.Interval = timer1.Interval - Convert.ToInt32(timer1.Interval / 10);
+            if (timer1.Interval == 10)
+            {
+                timer1.Enabled = false;
+                this.Visible = false;
+                Countdown count = new Countdown();
+                count.ShowDialog();
 
-            this.Close();
+                GameLoading game = new GameLoading();
+                game.Show();
+
+                this.Close();
+            }
+            pictureBox1.Image = coin[i];
         }
     }
 }
